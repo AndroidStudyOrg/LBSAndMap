2535c248e1e26bb1c05ea70bf5faf8920e5b3432
wbham <wbham97@naver.com>
2023-11-05 05:10:15 +0900

    initialsetting - viewBinding

---
+++ app/src/main/java/org/shop/lbsandmap/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.shop.lbsandmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.shop.lbsandmap.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
