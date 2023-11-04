87f45f8e10128c5e3308385fa8a4d20b97f10802
wbham <wbham97@naver.com>
2023-11-05 06:23:10 +0900

    [Feat] - FusedLocationSource로 현재 위치 표시
    - gradle에 FusedLocationSource dependency 추가
    - request code 상수 추가

---
+++ app/src/main/java/org/shop/lbsandmap/Constants.kt
@@ -0,0 +1,3 @@
+package org.shop.lbsandmap
+
+const val LOCATION_PERMISSION_REQUEST_CODE = 1000
--- app/src/main/java/org/shop/lbsandmap/MainActivity.kt
+++ app/src/main/java/org/shop/lbsandmap/MainActivity.kt
@@ -3,19 +3,26 @@ package org.shop.lbsandmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.UiThread
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import org.shop.lbsandmap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         setMap()
     }
@@ -32,6 +39,23 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.uiSettings.isLocationButtonEnabled = true
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
