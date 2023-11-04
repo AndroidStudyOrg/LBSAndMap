af99e25849c5e25d4c60fa552d93fa34904acdeb
wbham <wbham97@naver.com>
2023-11-05 06:05:23 +0900

    [Feat] - 네이버 지도 표현
    - 네이버 sdk maven을 프로젝트 수준 gradle에서 setting.gradle로 변경
    - MainActivity에서 네이버 지도 보여주는 것 활성화

--- app/src/main/java/org/shop/lbsandmap/MainActivity.kt
+++ app/src/main/java/org/shop/lbsandmap/MainActivity.kt
@@ -2,9 +2,13 @@ package org.shop.lbsandmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import org.shop.lbsandmap.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        setMap()
+    }
+
+    private fun setMap() {
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_naver_map) as MapFragment?
+                ?: MapFragment.newInstance().also {
+                    supportFragmentManager.beginTransaction().add(R.id.fragment_naver_map, it)
+                        .commit()
+                }
+        mapFragment.getMapAsync(this)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+
     }
 }
